cd15ac60e9ad99e27c654a4cf01e85d1ed74179e
JuanM134 <69129238+JuanM134@users.noreply.github.com>
2021-08-05 13:32:56 -0400

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const botonHamburguesa = document.getElementById('hamburguesa')
-const navList = document.getElementById('nav-list')
-
-function toggleButton(){
-    navList.classList.toggle('show')
-}
-
-botonHamburguesa.addEventListener('click',toggleButton)
